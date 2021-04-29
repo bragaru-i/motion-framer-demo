78c939b68704883bf91829ce21a39ffe5fd2f377
fargusmd <bragaruion@gmail.com>
2021-04-29 18:05:47 +0300

    Section 1 adding basic exit animation

--- src/sections/Section1.jsx
+++ src/sections/Section1.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import {motion} from 'framer-motion';
+
+import useObserverHook from '../hooks/useObserverHook'
+import {MAIN_DELAY} from '../constants'
+
+const drawerVariants = {
+    hidden :{
+        opacity:1,
+    },
+    visible: {
+        opacity:0,
+        display :"none"
+    },
+    exit: {
+        width: ["0%","100%"],
+        backgroundColor:"red",
+        opacity: [1,1,0]
+    }
+}
 
 const Section1 = () => {
+
+    // using our custom hook to get the current position in viewport and a ref for our section
+    const {ref, variant} = useObserverHook(MAIN_DELAY, true);
+    
     return (
-        <section id="section1">
+        <section ref ={ref} id="section1">
             Section 1
+            <motion.div animate={variant} variants={drawerVariants}/>          
         </section>
     )
 }
